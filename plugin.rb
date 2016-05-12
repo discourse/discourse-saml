1b40432c316cb12b2be9fa7e879f3bcaac8f5a20
Robin Ward <robin.ward@gmail.com>
2016-05-12 16:22:42 -0400

    More SAML Request features

--- plugin.rb
+++ plugin.rb
@@ -75,6 +75,11 @@ if request_method == 'post'
         end
 
         settings.compress_request = false
+        settings.passive = false
+        settings.issuer = 'discourse'
+        settings.assertion_consumer_service_url = Discourse.base_url + "/auth/saml/callback"
+        settings.name_identifier_format = "urn:oasis:names:tc:SAML:2.0:protocol"
+
         saml_params = authn_request.create_params(settings, {})
         @saml_req = saml_params['SAMLRequest']
 
