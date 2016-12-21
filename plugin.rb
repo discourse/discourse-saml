1a3f0a1d12d5c733513271c6cd27824a1f69c9c0
Matthew Purcell <matthew@CARBON.local>
2016-12-21 15:03:35 +1100

    Added full screen login setting

--- plugin.rb
+++ plugin.rb
@@ -151,4 +151,5 @@ auth_provider :title => button_title,
               :frame_width => 600,
               :frame_height => 380,
               :background_color => '#003366',
+              :full_screen_login => GlobalSetting.try(:saml_full_screen_login) || false,
               :custom_url => request_method == 'post' ? "/discourse_saml" : nil
