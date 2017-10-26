102fd87535098a7d198cd0396438628018d3086a
Arpit Jalan <arpit@techapj.com>
2017-10-26 12:22:42 +0530

    merge changes into master

--- plugin.rb
+++ plugin.rb
@@ -22,7 +22,14 @@ class SamlAuthenticator < ::Auth::OAuth2Authenticator
                       :idp_cert => GlobalSetting.try(:saml_cert),
                       :attribute_statements => { :nickname => ['screenName'] },
                       :assertion_consumer_service_url => Discourse.base_url + "/auth/saml/callback",
-                      :custom_url => (GlobalSetting.try(:saml_request_method) == 'post') ? "/discourse_saml" : nil
+                      :custom_url => (GlobalSetting.try(:saml_request_method) == 'post') ? "/discourse_saml" : nil,
+                      :certificate => GlobalSetting.try(:saml_sp_certificate),
+                      :private_key => GlobalSetting.try(:saml_sp_private_key),
+                      :security => {
+                        authn_requests_signed: GlobalSetting.try(:saml_authn_requests_signed) ? true : false,
+                        want_assertions_signed: GlobalSetting.try(:saml_want_assertions_signed) ? true : false,
+                        signature_method: XMLSecurity::Document::RSA_SHA1
+                      }
   end
 
   def after_authenticate(auth)
@@ -32,6 +39,11 @@ class SamlAuthenticator < ::Auth::OAuth2Authenticator
       ::PluginStore.set("saml", "saml_last_auth", auth.inspect)
       ::PluginStore.set("saml", "saml_last_auth_raw_info", auth.extra[:raw_info].inspect)
       ::PluginStore.set("saml", "saml_last_auth_extra", auth.extra.inspect)
+    end
+
+    if GlobalSetting.try(:saml_debug_auth)
+      log("saml_auth_info: #{auth[:info].inspect}")
+      log("saml_auth_extra: #{auth.extra.inspect}")
     end
 
     uid = auth[:uid]
@@ -52,12 +64,16 @@ class SamlAuthenticator < ::Auth::OAuth2Authenticator
       result.skip_email_validation = true
     end
 
-    current_info = ::PluginStore.get("saml", "saml_user_#{uid}")
-    if current_info
-      result.user = User.where(id: current_info[:user_id]).first
+    saml_user_info = ::PluginStore.get("saml", "saml_user_#{uid}")
+    if saml_user_info
+      result.user = User.where(id: saml_user_info[:user_id]).first
     end
 
-    result.user ||= User.where(email: Email.downcase(result.email)).first
+    result.user ||= User.find_by_email(result.email)
+
+    if saml_user_info.nil? && result.user
+      ::PluginStore.set("saml", "saml_user_#{uid}", {user_id: result.user.id })
+    end
 
     if GlobalSetting.try(:saml_clear_username) && result.user.blank?
       result.username = ''
@@ -67,25 +83,40 @@ class SamlAuthenticator < ::Auth::OAuth2Authenticator
       result.omit_username = true
     end
 
-    sync_groups(result.user, auth) unless result.user.blank?
-
     result.extra_data = { saml_user_id: uid }
+
+    if GlobalSetting.try(:saml_sync_groups)
+      groups = auth.extra[:raw_info].attributes['memberOf']
+
+      if result.user.blank?
+        result.extra_data[:saml_groups] = groups
+      else
+        sync_groups(result.user, groups)
+      end
+    end
+
+    sync_email(result.user, Email.downcase(result.email)) if GlobalSetting.try(:saml_sync_email) && result.user.present? && result.user.email != Email.downcase(result.email)
+
     result
+  end
+
+  def log(info)
+    Rails.logger.warn("SAML Debugging: #{info}") if GlobalSetting.try(:saml_debug_auth)
   end
 
   def after_create_account(user, auth)
     ::PluginStore.set("saml", "saml_user_#{auth[:extra_data][:saml_user_id]}", {user_id: user.id })
 
-    sync_groups(user, auth)
+    sync_groups(user, auth[:extra_data][:saml_groups])
   end
 
-  def sync_groups(user, auth)
+  def sync_groups(user, saml_groups)
 
-    return unless GlobalSetting.try(:saml_sync_groups) && GlobalSetting.try(:saml_sync_groups_list) && auth.extra.present? && auth.extra[:raw_info].present?
+    return unless GlobalSetting.try(:saml_sync_groups) && GlobalSetting.try(:saml_sync_groups_list) && saml_groups.present?
 
     total_group_list = GlobalSetting.try(:saml_sync_groups_list).split('|')
 
-    user_group_list = auth.extra[:raw_info].attributes['memberOf']
+    user_group_list = saml_groups
 
     groups_to_add = Group.where(name: total_group_list & user_group_list)
 
@@ -97,6 +128,16 @@ class SamlAuthenticator < ::Auth::OAuth2Authenticator
 
     groups_to_remove.each do |group|
       group.remove user
+    end
+  end
+
+  def sync_email(user, email)
+    return unless GlobalSetting.try(:saml_sync_email)
+
+    existing_user = User.find_by_email(email)
+    if email =~ EmailValidator.email_regex && existing_user.nil?
+      user.email = email
+      user.save
     end
   end
 
