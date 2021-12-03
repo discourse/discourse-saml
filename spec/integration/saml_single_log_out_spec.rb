8f43b51e991025fc8563b982d1f89091ad44321f
David Taylor <david@taylorhq.com>
2021-12-03 18:52:58 +0000

    DEV: Use plugin API for logout redirect, and add spec

---
+++ spec/integration/saml_single_log_out_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe "SAML Single Log Out" do
+  let(:user) { Fabricate(:user) }
+
+  it "does nothing when SLO is not configured" do
+    sign_in(user)
+    delete "/session/#{user.username}", xhr: true
+    expect(response.status).to eq(200)
+    expect(response.parsed_body["redirect_url"]).to eq("/")
+  end
+
+  it "redirects to the omniauth route when SLO is configured" do
+    global_setting :saml_slo_target_url, "https://example.com/slo-target"
+
+    sign_in(user)
+    delete "/session/#{user.username}", xhr: true
+    expect(response.status).to eq(200)
+    expect(response.parsed_body["redirect_url"]).to eq("/auth/saml/spslo")
+  end
+end
