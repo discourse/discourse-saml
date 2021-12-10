discourse/discourse-saml

commit e9f9150b012746f31efd2da3bbf13c7af8489c49

FEATURE: Allow the plugin to be configured using site settings
GlobalSettings configured via environment variables will continue to take precendence, so this change is backwards compatible.

- `.presence` is added to string values, since 'unset' site settings are never `nil`
- saml_force_domains is split on `|` and `,` because site settings expect a `|`, while the old global setting expected a `,`
- Specs are migrated to use SiteSettings, because GlobalSettings do not override SiteSettings in test mode

lib/saml_authenticator.rb
@@ -64,20 +64,20 @@ class SamlAuthenticator < ::Auth::OAuth2Authenticator
     strategy.options.deep_merge!(
       issuer: SamlAuthenticator.saml_base_url,
       idp_sso_target_url: setting(:target_url),
-      idp_slo_target_url: setting(:slo_target_url),
+      idp_slo_target_url: setting(:slo_target_url).presence,
       slo_default_relay_state: SamlAuthenticator.saml_base_url,
-      idp_cert_fingerprint: setting(:cert_fingerprint),
+      idp_cert_fingerprint: setting(:cert_fingerprint).presence,
       idp_cert_fingerprint_algorithm: setting(:cert_fingerprint_algorithm),
-      idp_cert: setting(:cert),
-      idp_cert_multi: setting(:cert_multi),
+      idp_cert: setting(:cert).presence,
+      idp_cert_multi: setting(:cert_multi).presence,
       request_attributes: request_attributes,
       attribute_statements: attribute_statements,
       assertion_consumer_service_url: SamlAuthenticator.saml_base_url + "/auth/#{name}/callback",
       single_logout_service_url: SamlAuthenticator.saml_base_url + "/auth/#{name}/slo",
-      name_identifier_format: setting(:name_identifier_format),
+      name_identifier_format: setting(:name_identifier_format).presence,
       request_method: (setting(:request_method)&.downcase == 'post') ? "POST" : "GET",
-      certificate: setting(:sp_certificate),
-      private_key: setting(:sp_private_key),
+      certificate: setting(:sp_certificate).presence,
+      private_key: setting(:sp_private_key).presence,
       security: {
         authn_requests_signed: !!setting(:authn_requests_signed),
         want_assertions_signed: !!setting(:want_assertions_signed),
@@ -126,7 +126,7 @@ class SamlAuthenticator < ::Auth::OAuth2Authenticator
     result.username = begin
       if attributes.present?
         username = attributes['screenName'].try(:first)
-        username = attributes['uid'].try(:first) if setting(:use_uid)
+        username = attributes['uid'].try(:first) if setting(:use_attributes_uid)
       end
 
       username ||= begin

spec/saml_authenticator_spec.rb
@@ -70,7 +70,7 @@ describe SamlAuthenticator do
     end
 
     it 'defaults email_valid to false if saml_default_emails_valid is false' do
-      GlobalSetting.stubs(:saml_default_emails_valid).returns(false)
+      SiteSetting.saml_default_emails_valid = false
 
       hash = OmniAuth::AuthHash.new(
         uid: @uid,
@@ -86,7 +86,7 @@ describe SamlAuthenticator do
     end
 
     it 'defaults email_valid based on saml_validate_email_fields setting' do
-      GlobalSetting.stubs(:saml_validate_email_fields).returns("customers")
+      SiteSetting.saml_validate_email_fields = "customers"
 
       hash = auth_hash(
         'memberOf' => %w(Customers Employees)
@@ -98,7 +98,7 @@ describe SamlAuthenticator do
     end
 
     it 'stores additional request attributes to user custom fields' do
-      GlobalSetting.stubs(:saml_request_attributes).returns("department|title")
+      SiteSetting.saml_request_attributes = "department|title"
 
       hash = auth_hash(
         'department' => ["HR", "Manager"],
@@ -106,13 +106,13 @@ describe SamlAuthenticator do
       )
 
       result = @authenticator.after_authenticate(hash)
-      GlobalSetting.saml_request_attributes.split("|").each do |name|
+      SiteSetting.saml_request_attributes.split("|").each do |name|
         expect(result.user.custom_fields["saml_#{name}"]).to eq(hash.extra.raw_info.attributes[name].join(","))
       end
     end
 
     it 'syncs user fields based on `saml_user_field_statements` environment variable' do
-      GlobalSetting.stubs(:saml_user_field_statements).returns("department:2|title:3")
+      SiteSetting.saml_user_field_statements = "department:2|title:3"
 
       hash = auth_hash(
         'department' => ["HR", "Manager"],
@@ -122,14 +122,14 @@ describe SamlAuthenticator do
       result = @authenticator.after_authenticate(hash)
       attrs = hash.extra.raw_info.attributes
 
-      GlobalSetting.saml_user_field_statements.split("|").each do |statement|
+      SiteSetting.saml_user_field_statements.split("|").each do |statement|
         key, id = statement.split(":")
         expect(result.user.custom_fields["user_field_#{id}"]).to eq(attrs[key].join(","))
       end
     end
 
     it 'syncs user locale' do
-      GlobalSetting.stubs(:saml_sync_locale).returns(true)
+      SiteSetting.saml_sync_locale = true
       user_locale = "fr"
 
       hash = auth_hash(
@@ -143,7 +143,7 @@ describe SamlAuthenticator do
     end
 
     it 'should get uid value from extra attributes param' do
-      GlobalSetting.stubs(:saml_use_attributes_uid).returns("true")
+      SiteSetting.saml_use_attributes_uid = true
 
       hash = auth_hash('uid' => ["789"])
 
@@ -152,7 +152,7 @@ describe SamlAuthenticator do
     end
 
     it 'creates new account automatically' do
-      GlobalSetting.stubs(:saml_auto_create_account).returns(true)
+      SiteSetting.saml_auto_create_account = true
       name = "John Doe"
       email = "johndoe@example.com"
 
@@ -194,7 +194,7 @@ describe SamlAuthenticator do
       }
 
       it 'should be equal to uid' do
-        GlobalSetting.stubs(:saml_use_uid).returns(true)
+        SiteSetting.saml_use_attributes_uid = true
 
         result = @authenticator.after_authenticate(hash)
         expect(result.username).to eq(@uid.to_s)
@@ -261,7 +261,7 @@ describe SamlAuthenticator do
       let(:new_email) { "johndoe@demo.com" }
 
       before do
-        GlobalSetting.stubs(:saml_sync_email).returns(true)
+        SiteSetting.saml_sync_email = true
         @hash = auth_hash({})
         @hash.info.email = new_email
       end
@@ -280,7 +280,7 @@ describe SamlAuthenticator do
       let(:group_names) { ["group_1", "Group_2", "GROUP_3", "group_4"] }
 
       before do
-        GlobalSetting.stubs(:saml_sync_groups).returns(true)
+        SiteSetting.saml_sync_groups = true
         @groups = group_names.map { |name| Fabricate(:group, name: name.downcase) }
 
         @groups[3].add @user
@@ -297,7 +297,7 @@ describe SamlAuthenticator do
       end
 
       it 'sync users to the given groups within scope' do
-        GlobalSetting.stubs(:saml_sync_groups_list).returns(group_names[1..3].join("|"))
+        SiteSetting.saml_sync_groups_list = group_names[1..3].join("|")
 
         result = @authenticator.after_authenticate(@hash)
         expect(result.user.groups.pluck(:name)).to match_array(group_names[1..2].map(&:downcase))
@@ -309,8 +309,8 @@ describe SamlAuthenticator do
       let(:group_names_ldap) { ["cn=group_1,cn=groups,dc=example,dc=com", "cn=Group_2,cn=groups,dc=example,dc=com", "cn=GROUP_3,cn=groups,dc=example,dc=com", "cn=group_4,cn=groups,dc=example,dc=com"] }
 
       before do
-        GlobalSetting.stubs(:saml_sync_groups).returns(true)
-        GlobalSetting.stubs(:saml_groups_ldap_leafcn).returns(true)
+        SiteSetting.saml_sync_groups = true
+        SiteSetting.saml_groups_ldap_leafcn = true
         @groups = group_names.map { |name| Fabricate(:group, name: name.downcase) }
 
         @groups[3].add @user
@@ -327,7 +327,7 @@ describe SamlAuthenticator do
       end
 
       it 'sync users to the given groups within scope' do
-        GlobalSetting.stubs(:saml_sync_groups_list).returns(group_names[1..3].join("|"))
+        SiteSetting.saml_sync_groups_list = group_names[1..3].join("|")
 
         result = @authenticator.after_authenticate(@hash)
         expect(result.user.groups.pluck(:name)).to match_array(group_names[1..2].map(&:downcase))
@@ -338,8 +338,8 @@ describe SamlAuthenticator do
       let(:group_names) { ["group_1", "Group_2", "GROUP_3", "group_4"] }
 
       before do
-        GlobalSetting.stubs(:saml_sync_groups).returns(true)
-        GlobalSetting.stubs(:saml_groups_fullsync).returns(true)
+        SiteSetting.saml_sync_groups = true
+        SiteSetting.saml_groups_fullsync = true
         @groups = group_names.map { |name| Fabricate(:group, name: name.downcase) }
 
         @hash = auth_hash(
@@ -362,7 +362,7 @@ describe SamlAuthenticator do
 
     describe "set moderator" do
       before do
-        GlobalSetting.stubs(:saml_sync_moderator).returns(true)
+        SiteSetting.saml_sync_moderator = true
       end
 
       it 'user should be a moderator (default param)' do
@@ -374,7 +374,7 @@ describe SamlAuthenticator do
       end
 
       it 'user should be a moderator (using specified saml_moderator_attribute)' do
-        GlobalSetting.stubs(:saml_moderator_attribute).returns('is_a_moderator')
+        SiteSetting.saml_moderator_attribute = 'is_a_moderator'
         hash = auth_hash(
           'is_a_moderator' => ['true'],
         )
@@ -385,7 +385,7 @@ describe SamlAuthenticator do
 
     describe "set admin" do
       before do
-        GlobalSetting.stubs(:saml_sync_admin).returns(true)
+        SiteSetting.saml_sync_admin = true
       end
 
       it 'user should be an admin (default param)' do
@@ -397,7 +397,7 @@ describe SamlAuthenticator do
       end
 
       it 'user should be an admin (using specified saml_admin_attribute)' do
-        GlobalSetting.stubs(:saml_admin_attribute).returns('is_an_admin')
+        SiteSetting.saml_admin_attribute = 'is_an_admin'
         hash = auth_hash(
           'is_an_admin' => ['true'],
         )
@@ -408,7 +408,7 @@ describe SamlAuthenticator do
 
     describe "set trust_level" do
       before do
-        GlobalSetting.stubs(:saml_sync_trust_level).returns(true)
+        SiteSetting.saml_sync_trust_level = true
       end
 
       it 'user should have trust level 3 (default param)' do
@@ -421,7 +421,7 @@ describe SamlAuthenticator do
       end
 
       it 'user should have trust level 3 (using specified saml_trust_level_attribute)' do
-        GlobalSetting.stubs(:saml_trust_level_attribute).returns('my_trust_level')
+        SiteSetting.saml_trust_level_attribute = 'my_trust_level'
         hash = auth_hash(
           'my_trust_level' => ['3'],
         )
@@ -454,25 +454,25 @@ describe SamlAuthenticator do
       it "matches request_attributes count" do
         expect(@authenticator.request_attributes.count).to eq(4)
 
-        GlobalSetting.stubs(:saml_request_attributes).returns("company_name|mobile_number|name")
+        SiteSetting.saml_request_attributes = "company_name|mobile_number|name"
         expect(@authenticator.request_attributes.count).to eq(6)
       end
 
       it "matches attribute_statements count" do
         expect(@authenticator.attribute_statements.count).to eq(5)
 
-        GlobalSetting.stubs(:saml_attribute_statements).returns("email:emailAddress|company|name")
+        SiteSetting.saml_attribute_statements = "email:emailAddress|company|name"
         expect(@authenticator.attribute_statements.count).to eq(5)
         expect(@authenticator.attribute_statements["email"]).to eq(["email", "mail", "emailAddress"])
 
-        GlobalSetting.stubs(:saml_attribute_statements).returns("company_name:company,business|phone:mobile,contact_no")
+        SiteSetting.saml_attribute_statements = "company_name:company,business|phone:mobile,contact_no"
         expect(@authenticator.attribute_statements.count).to eq(7)
       end
     end
 
     context 'after_create_account' do
       it 'adds to group' do
-        GlobalSetting.stubs(:saml_sync_groups).returns(true)
+        SiteSetting.saml_sync_groups = true
         authenticator = SamlAuthenticator.new("saml", trusted: true)
         user = Fabricate(:user, email: 'realgoogleuser@gmail.com')
         group = Fabricate(:group)

config/locales/client.en.yml
@@ -4,3 +4,8 @@ en:
       saml:
         name: "SAML"
         title: with SAML
+  admin_js:
+    admin:
+      site_settings:
+        categories: 
+          saml: SAML

config/locales/server.en.yml
@@ -1,3 +1,63 @@
 en:
   login:
     use_saml_auth: "Please use the company SSO to login with your account."
+  site_settings:
+    saml_enabled: Enable SAML authentication
+
+    saml_target_url: Target URL of the SAML Identity Provider (required)
+    saml_slo_target_url: Target URL for SAML Single Log Out
+
+    saml_name_identifier_format: If provided, will request a specific NameID (UID) format from the identity provider.
+
+    saml_cert: X.509 public certificate of the SAML identity provider (either this, or saml_cert_fingerprint, are required)
+    saml_cert_fingerprint: The X.509 public certificate fingerprint of the SAML identity provider (either this, or `saml_cert`, are required)
+    saml_cert_fingerprint_algorithm: Which algorithm should be used for SAML certificate fingerprinting?
+    saml_cert_multi: A secondary X.509 public certificate of the SAML identity provider. Useful during certificate rotations
+
+    saml_request_method: The HTTP method used when directing the user to the Identity Provider
+
+    saml_sp_certificate: SAML Service Provider X.509 certficate. Used to sign messages once enabled via the `saml_*_signed` settings"
+    saml_sp_private_key: SAML Service Provider X.509 private key. Used to sign messages once enabled via the `saml_*_signed` settings"
+    saml_authn_requests_signed: Enable Service Provider signatures for AuthNRequest
+    saml_want_assertions_signed: Enable Service Provider signatures for Assertions
+    saml_logout_requests_signed: Enable Service Provider signatures for SP-initiated logout
+    saml_logout_responses_signed: Enable Service Provider signatures for IDP-initiated logout responses
+
+    saml_request_attributes: A list of additional attributes which should be fetched from the service provider. `email`, `name`, `first_name`,  and `last_name` are always fetched.
+    saml_attribute_statements: Custom mappings of fields to their source SAML attributes. In the format `field:samlAttr1,samlAttr2`.
+    saml_use_attributes_uid: Use the 'uid' attribute as the unique user identifier instead of the default `name_id` field.
+
+    saml_skip_email_validation: Skip syntax validation of emails from the SAML IDP
+    saml_validate_email_fields: If any of these values are present in the `memberOf` attribute, then the email should be considered valid/verified
+    saml_default_emails_valid: "Consider SAML emails to be verified? Warning: this should only be `true` if you trust the IDP to verify email ownership"
+
+    saml_clear_username: Ignore the username from the SAML result
+    saml_omit_username: Prevent the user from changing the SAML username during signup
+    saml_auto_create_account: Skip the registration popup during signup with a SAML account
+
+    saml_sync_groups: Synchronize SAML groups with Discourse
+    saml_groups_fullsync: Should the assigned groups be completely synced including adding AND removing groups based on the IDP? Defaults to false. If set to true, `saml_sync_groups_list` and SAML attribute `groups_to_add`/`groups_to_remove` are not used.
+    saml_groups_attribute: The SAML attribute which contains group names
+    saml_groups_ldap_leafcn: If your IDP transmits `cn=groupname,cn=groups,dc=example,dc=com` you can set this to true to use only `groupname`. This is useful if you want to keep the standard group name length of Discourse (20 characters).
+    saml_sync_groups_list: If provided, these are the only Discourse groups which will have their membership controlled by SAML. If blank, all groups_to_add/groups_to_remove are used.
+
+    saml_user_field_statements: If provided, user fields will be set based on SAML attributes. Each entry should be in the format `saml_attribute_name:discourse_field_id`
+
+    saml_sync_email: Update the user email on every SAML login
+
+    saml_sync_moderator: Sync moderator status from SAML result?
+    saml_moderator_attribute: The SAML attribute which contains the moderator boolean
+    saml_sync_admin: Sync admin status from SAML result?
+    saml_admin_attribute: The SAML attribute which contains the admin boolean
+
+    saml_sync_trust_level: Set user trust level from SAML result
+    saml_trust_level_attribute: The SAML attribute which contains the trust level integer
+
+    saml_sync_locale: Set user locale from SAML result
+    saml_locale_attribute: The SAML attribute which contains the locale name
+
+    saml_forced_domains: Users with email addresses on these domains will be forced to use the SAML flow. They will be blocked from other login methods.
+
+    saml_log_auth: Store raw data about authentications in the `plugin_store_rows` database table.
+    saml_debug_auth: Enable debug logging to `/logs`
+    saml_base_url: Override the base URL for the Service Provider. Defaults to the forum base URL.

config/settings.yml
@@ -1,0 +1,89 @@
+saml:
+  saml_enabled: false
+
+  saml_target_url: ""
+  saml_slo_target_url: ""
+
+  saml_name_identifier_format: ""
+
+  saml_cert:
+    default: ""
+    textarea: true
+  saml_cert_fingerprint: ""
+  saml_cert_fingerprint_algorithm:
+    type: enum
+    default: SHA1
+    choices:
+      - SHA1
+      - SHA256
+      - SHA384
+      - SHA512
+  saml_cert_multi:
+    default: ""
+    textarea: true
+
+  saml_request_method:
+    type: enum
+    default: GET
+    choices:
+      - GET
+      - POST
+  saml_sp_certificate:
+    default: ""
+    textarea: true
+  saml_sp_private_key:
+    default: ""
+    textarea: true
+  saml_authn_requests_signed: false
+  saml_want_assertions_signed: false
+  saml_logout_requests_signed: false
+  saml_logout_responses_signed: false
+
+  saml_request_attributes:
+    type: list
+    default: ""
+  saml_attribute_statements:
+    type: list
+    default: ""
+  saml_use_attributes_uid: false
+
+  saml_skip_email_validation: false
+  saml_validate_email_fields:
+    type: list
+    default: ""
+  saml_default_emails_valid: true
+
+  saml_clear_username: false
+  saml_omit_username: false
+  saml_auto_create_account: false
+
+  saml_sync_groups: false
+  saml_groups_fullsync: false
+  saml_groups_attribute: 'memberOf'
+  saml_groups_ldap_leafcn: false
+  saml_sync_groups_list:
+    type: list
+    default: ""
+
+  saml_user_field_statements:
+    type: list
+    default: ""
+
+  saml_sync_email: false
+  
+  saml_sync_moderator: false
+  saml_moderator_attribute: "isModerator"
+  saml_sync_admin: false
+  saml_admin_attribute: "isAdmin"
+  saml_sync_trust_level: false
+  saml_trust_level_attribute: "trustLevel"
+  saml_sync_locale: false
+  saml_locale_attribute: 'locale'
+
+  saml_forced_domains:
+    type: list
+    default: ""
+
+  saml_log_auth: false
+  saml_debug_auth: false
+  saml_base_url: ""

plugin.rb
@@ -14,25 +14,36 @@ gem 'rexml', '3.2.5'
 gem 'ruby-saml', '1.13.0'
 gem "omniauth-saml", '1.9.0'
 
+if !GlobalSetting.try("saml_target_url")
+  enabled_site_setting :saml_enabled
+end
+
 on(:before_session_destroy) do |data|
   next if !DiscourseSaml.setting(:slo_target_url).present?
   data[:redirect_url] = Discourse.base_path + "/auth/saml/spslo"
 end
 
 module ::DiscourseSaml
+  def self.enabled?
+    # Legacy - we only check the enabled site setting
+    # if the environment-variables are **not** present
+    !!GlobalSetting.try("saml_target_url") || SiteSetting.saml_enabled
+  end
+
   def self.setting(key, prefer_prefix: "saml_")
-    if prefer_prefix == "saml_" && SiteSetting.has_setting?("saml_#{key}")
+    if prefer_prefix == "saml_"
       SiteSetting.get("saml_#{key}")
     else
-      GlobalSetting.try("#{prefer_prefix}#{key}") || GlobalSetting.try("saml_#{key}")
+      GlobalSetting.try("#{prefer_prefix}#{key}") || SiteSetting.get("saml_#{key}")
     end
   end
 
   def self.is_saml_forced_domain?(email)
+    return if !enabled?
     return if !DiscourseSaml.setting(:forced_domains).present?
     return if email.blank?
 
-    DiscourseSaml.setting(:forced_domains).split(",").each do |domain|
+    DiscourseSaml.setting(:forced_domains).split(/[,|]/).each do |domain|
       return true if email.end_with?("@#{domain}")
     end
 

spec/integration/saml_forced_domains_spec.rb
@@ -20,8 +20,9 @@ describe "SAML Forced Domains" do
   end
 
   before do
+    SiteSetting.saml_enabled = true
     OmniAuth.config.test_mode = true
-    global_setting :saml_target_url, "https://example.com/samltarget"
+    SiteSetting.saml_target_url = "https://example.com/samltarget"
   end
 
   describe "username/password login" do
@@ -34,7 +35,7 @@ describe "SAML Forced Domains" do
     end
 
     it "blocks logins for blocked domains" do
-      global_setting :saml_forced_domains, "samlonly.example.com"
+      SiteSetting.saml_forced_domains = "samlonly.example.com"
       post "/session.json", params: {
         login: saml_user.username, password: password
       }
@@ -62,14 +63,14 @@ describe "SAML Forced Domains" do
     end
 
     it "blocks login for blocked domains" do
-      global_setting :saml_forced_domains, "samlonly.example.com"
+      SiteSetting.saml_forced_domains = "samlonly.example.com"
       post "/u/email-login.json", params: { login: saml_user.email }
       expect(response.status).to eq(403)
       expect_not_enqueued_with(job: :critical_user_email)
     end
 
     it "allows login for other domains" do
-      global_setting :saml_forced_domains, "samlonly.example.com"
+      SiteSetting.saml_forced_domains = "samlonly.example.com"
       post "/u/email-login.json", params: { login: other_user.email }
       expect(response.status).to eq(200)
       expect_job_enqueued(job: :critical_user_email, args: {
@@ -103,7 +104,7 @@ describe "SAML Forced Domains" do
     end
 
     it "blocks login for blocked domains" do
-      global_setting :saml_forced_domains, "samlonly.example.com"
+      SiteSetting.saml_forced_domains = "samlonly.example.com"
       get "/auth/google_oauth2/callback"
       expect(response.status).to eq(200)
       expect(response.body).to include(I18n.t("login.use_saml_auth"))
@@ -111,14 +112,14 @@ describe "SAML Forced Domains" do
     end
 
     it "allows SAML login for blocked domains" do
-      global_setting :saml_forced_domains, "samlonly.example.com"
+      SiteSetting.saml_forced_domains = "samlonly.example.com"
       get "/auth/saml/callback"
       expect(response.status).to eq(302)
       expect(session[:current_user_id]).to eq(saml_user.id)
     end
 
     it "allows login for other domains" do
-      global_setting :saml_forced_domains, "samlonly.example.com"
+      SiteSetting.saml_forced_domains = "samlonly.example.com"
       mock_auth.info.email = other_user.email
       get "/auth/google_oauth2/callback"
       expect(response.status).to eq(302)

spec/integration/saml_post_mode_spec.rb
@@ -4,19 +4,20 @@ require 'rails_helper'
 
 describe "SAML POST-mode functionality", type: :request do
   before do
+    SiteSetting.saml_enabled = true
     OmniAuth.config.test_mode = false
-    global_setting :saml_target_url, "https://example.com/samlidp"
+    SiteSetting.saml_target_url = "https://example.com/samlidp"
   end
 
   it "does not affect functionality when disabled" do
-    global_setting :saml_request_method, "GET"
+    SiteSetting.saml_request_method = "GET"
     post "/auth/saml"
     expect(response.status).to eq(302)
     expect(response.location).to start_with("https://example.com/samlidp")
   end
 
   it "serves an auto-submitting POST form when enabled" do
-    global_setting :saml_request_method, "POST"
+    SiteSetting.saml_request_method = "POST"
     post "/auth/saml"
     expect(response.status).to eq(200)
     expect(response.body).to have_tag(

commit 68561d0f43885a0acd0d327db92a160b5ca8c15d

FIX: Ignore empty `saml_base_url` site setting
Regression introduced in e9f9150b

lib/saml_authenticator.rb
@@ -369,7 +369,7 @@ class SamlAuthenticator < ::Auth::OAuth2Authenticator
   end
 
   def self.saml_base_url
-    DiscourseSaml.setting(:base_url) || Discourse.base_url
+    DiscourseSaml.setting(:base_url).presence || Discourse.base_url
   end
 
 end

spec/saml_authenticator_spec.rb
@@ -494,4 +494,15 @@ describe SamlAuthenticator do
       end
     end
   end
+
+  describe ".base_url" do
+    it "works" do
+      expect(SamlAuthenticator.saml_base_url).to eq("http://test.localhost")
+    end
+
+    it "can be overriden by a setting" do
+      SiteSetting.saml_base_url = "https://override.example.com"
+      expect(SamlAuthenticator.saml_base_url).to eq("https://override.example.com")
+    end
+  end
 end